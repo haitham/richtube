d556faf5592806c731f8df899458fb99f7332115
thedecimal <yaser.alashry@espace.com.eg>
2011-05-05 16:08:02 +0300

    new video page layout

--- public/javascripts/composer.js
+++ public/javascripts/composer.js
@@ -1,6 +1,28 @@
 $(function(){
+  $("#add_widget").hide()
+  var insertAction = function(action,timeLine){
+  }
+  
+  var getPlayerObject = function(){
+    return document.getElementById('video_container object');
+  }
+
 	$("#youtube_fetch").click(function(){
 		var videoEmbed = Util.getYoutubeEmbedFromUrl($("#youtube_url").val());
 		$("#video_container").html(videoEmbed);
+		$("#add_widget").show()
 	});
+	
+	$("#start_subtitle").live( "click" , function(){
+	  getPlayerObject().playVideo()
+  });
+  
+  $("#end_subtitle").live( "click" , function(){
+    getPlayerObject().pauseVideo()
+  });
+  
+  $("#save").live( "click" , function(){
+    alert("save_button")
+  });
+	
 });
